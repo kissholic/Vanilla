cef44403646748ad88e80822e5f6f1800605057e
kissholic <kissholicovo@outlook.com>
2024-02-03 16:19:10 +0800

    format code

--- test/Core/Coroutine/test_async_mutex.cpp
+++ test/Core/Coroutine/test_async_mutex.cpp
@@ -6,19 +6,17 @@
 #include <string>
 
 async_mutex mutex;
-std::set<std::string>
+std::set<std::string>;
 
-co_task<> add_item(std::string value)
-{
-    async_mutex_lock lock = co_await mutex.scoped_lock_async();
+co_task<> add_item(std::string value) {
+  async_mutex_lock lock = co_await mutex.scoped_lock_async();
 
-    values.insert(std::move(value));
+  values.insert(std::move(value));
 }
 
-int main()
-{
-    add_item("Hello");
-    add_item("world");
-    
-    return 0;
+int main() {
+  add_item("Hello");
+  add_item("world");
+
+  return 0;
 }
